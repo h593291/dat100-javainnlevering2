8414ee4df146cdecfcd8bc6576637df8deb3b43c
LEB <LEB@larserilandsmbp.lan>
2020-09-27 21:58:25 +0200

    Matriser

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -1,50 +1,82 @@
 package no.hvl.dat100.lab6.matriser;
 
 public class Matriser {
-
+	
 	// a)
-	public static void skrivUt(int[][] matrise) {
+	public static void skrivUtv1(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		System.out.print("[ ");
+		
+		for (int []i : matrise) {
+			
+			for (int j : i) {
+				System.out.print(j + " ");
+				
+			}
+								
+		}
+		
+		System.out.println("]");
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		
+		String returnStreng = "";
+		String a = "";
+		
+		for (int []i : matrise) {
+			
+			for (int j : i) {
+				a += j + " ";
+				
+			}
+			returnStreng += a + "\n";
+			a = "";
+		}
+		
+		return returnStreng;
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
+		int returnTab [][] = new int [matrise.length][matrise[0].length];
+		
+		for (int i = 0; i<matrise.length; i++) {
+			
+			for (int j = 0; j<matrise[0].length; j++) {
+				
+				returnTab[i][j] = matrise[i][j] * tall;
+				
+			}
+			
+		}
+		
+		return returnTab;
 	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
-	}
+		boolean svar = true;
+				
+		for (int i = 0; i<a.length; i++) {
+						
+				for (int j = 0; j<a[0].length; j++) {
+					
+					if (a[i][j]!=b[i][j]) {
+						
+						svar = false;
+					}
+					
+				}
+			}
+		
+		return svar;
+		}
 	
-	// e)
-	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
-	}
-
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
-	}
 }
+	
